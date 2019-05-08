4be301a49eb10a37e5e0e3ef248b44a79db7da1b
Artem <ar4iius1998@gmail.com>
2019-05-08 12:47:20 +0300

    init

---
+++ main/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Profile(models.Model):
+    pass
+
+
+class Car:
+    pass
+
+
+class Links:
+    pass
+
+
+class Brand:
+    pass
+
+
+class Model:
+    pass
+
+
+class Location:
+    pass
+
+
