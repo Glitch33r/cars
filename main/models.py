4ca55c147c23ce5d13cd985c6a1c0e636f3de883
Artem <ar4iius1998@gmail.com>
2019-05-08 16:18:20 +0300

    init

--- main/models.py
+++ main/models.py
@@ -1,27 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=13, blank=False)
+    expiredAt = models.DateTimeField(blank=True)
+
     pass
 
 
-class Car:
+class Filter(models.Model):
+    GEAR_BOX_TYPE = (
+        ('ANY', ''),
+        ('AUTO', ''),
+        ('MANUAL', ''),
+    )
+
+    SELL_TYPE = (
+        ('ACT', ''),
+        ('SOLD', ''),
+        ('ACT_SOLD', ''),
+    )
+
+    CLEARED_TYPE = (
+        ('CLR', ''),
+        ('NOT_CLR', ''),
+        ('ANY', ''),
+    )
+
+    ACCIDENT_TYPE = (
+        ('NO', ''),
+        ('YES', ''),
+        ('ANY', ''),
+    )
     pass
 
 
-class Links:
-    pass
-
-
-class Brand:
-    pass
-
-
-class Model:
-    pass
-
-
-class Location:
-    pass
-
+# class Model:
+#     pass
+#
+# class Mark:
+#     pass
+#
+#
+# class Location:
+#     pass
+#
 
