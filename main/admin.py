1180ac6901f5a74934d844aeef54e69238107cfa
DMK <dmkshift@gmail.com>
2019-05-08 23:55:08 +0300

    added location, mark, model, profile, filter-form

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Profile)
+
+
+class MarkInline(admin.TabularInline):
+    model = Mark
+    extra = 0
+
+
+class ModelAdmin(admin.ModelAdmin):
+    inlines = [MarkInline]
+
+
+admin.site.register(Model, ModelAdmin)
+admin.site.register(Location)
+
--- main/models.py
+++ main/models.py
@@ -7,44 +7,79 @@ class Profile(models.Model):
     phone = models.CharField(max_length=13, blank=False)
     expiredAt = models.DateTimeField(blank=True)
 
+    def __str__(self):
+        return self.user.email
+
+    class Meta:
+        verbose_name_plural = 'Profiles'
+
     pass
 
 
-class Filter(models.Model):
+class FilterForm(models.Model):
     GEAR_BOX_TYPE = (
-        ('ANY', ''),
-        ('AUTO', ''),
-        ('MANUAL', ''),
+        ('ANY', 'Любая КПП'),
+        ('AUTO', 'Автоматическая'),
+        ('MANUAL', 'Механическая'),
     )
 
     SELL_TYPE = (
-        ('ACT', ''),
-        ('SOLD', ''),
-        ('ACT_SOLD', ''),
+        ('ACT', 'Актуальные(только в Продаже)'),
+        ('SOLD', 'Проданные'),
+        ('ACT_SOLD', 'Актуальные и проданные'),
     )
 
     CLEARED_TYPE = (
-        ('CLR', ''),
-        ('NOT_CLR', ''),
-        ('ANY', ''),
+        ('CLR', 'Растаможенные'),
+        ('NOT_CLR', 'Не растаможенные'),
+        ('ANY', 'Любая реестрация'),
     )
 
     ACCIDENT_TYPE = (
-        ('NO', ''),
-        ('YES', ''),
-        ('ANY', ''),
+        ('NO', 'без ДТП'),
+        ('YES', 'после ДТП'),
+        ('ANY', 'с ДТП и без'),
     )
-    pass
+
+    model = models.ForeignKey('Model', null=True, on_delete=models.SET_NULL)
+    mark = models.ForeignKey('Mark', null=True, on_delete=models.SET_NULL)
+    location = models.ForeignKey('Location', null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        managed = False
 
 
-# class Model:
-#     pass
-#
-# class Mark:
-#     pass
-#
-#
-# class Location:
-#     pass
-#
+class Model(models.Model):
+    name = models.CharField(max_length=128, blank=False)
 
+    def __str__(self):
+        return self.name
+
+
+class Mark(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+    model = models.ForeignKey(Model, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    region = models.CharField(max_length=128, blank=False)
+
+    def __str__(self):
+        return self.region
+
+
+class Fuel(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Body(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+
+    def __str__(self):
+        return self.name
