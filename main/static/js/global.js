7313d554a9b20d12a25aa6eb7170ce9b28c8bde0
swaldom <svichkar@i.ua>
2019-06-25 12:51:21 +0300

    feat: home.html

---
+++ main/static/js/global.js
@@ -0,0 +1,130 @@
+$(function(){
+    //glue nav
+    var navPos,
+        winPos,
+        navHeight;
+    function refreshVar(){
+        navPos = $("header").offset().top;
+        navHeight = $("header").outerHeight(true);
+    }
+    refreshVar()
+    $(window).scroll(function(){
+        winPos = $(window).scrollTop();
+        if(winPos >= navPos){
+            $("header").addClass("fixed shadow");
+            $(".clone-nav").show();
+        }
+        if(winPos == 0){
+            $("header").removeClass("fixed shadow");
+            $(".clone-nav").hide();
+        }
+    });
+
+    $("<div class='clone-nav'></div>").insertBefore("header").css("height", navHeight).hide();
+
+    $('.yearselect').yearselect();
+    $('.yrselectdesc').yearselect({step: 5, order: 'desc'});
+    $('.yrselectasc').yearselect({order: 'asc'});
+
+
+
+    $('.toggle').click(function(e) {
+        e.preventDefault();
+    
+      var $this = $(this);
+    
+      if ($this.next().hasClass('show')) {
+          $this.next().removeClass('show');
+          $this.parent().removeClass('show');
+          $this.next().slideUp(350);
+
+      } else {
+          $this.parent().parent().find('li .inner').removeClass('show');
+          $this.parent().parent().find('li .inner').slideUp(350);
+          $this.next().toggleClass('show');
+          $this.parent().toggleClass('show');
+          $this.next().slideToggle(350);
+      }
+      if ($this.parent().hasClass('show')){
+        $this.next().on("mouseleave", function(){
+            $this.next().removeClass('show');
+            $this.parent().removeClass('show');
+            $this.next().slideUp(350);
+        })
+      } 
+
+  });
+
+//price
+  $('.price_pick input').bind('keypress', function (event) {
+    var regex = new RegExp("^[0-9]+$");
+    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)) {
+       event.preventDefault();
+       return false;
+    }
+});
+
+// button show_more/hide 
+    $(".about_car").elimore({
+        maxLength: 340
+        });
+    var about_w = $(".description").width();
+    $(".about_car").width(about_w);
+    $(window).resize(function(){
+        refreshVar();
+        var about_w = $(".description").width();
+        $(".about_car").width(about_w);
+    })
+
+
+
+
+    //pit to top
+    $(".star").on("click", function(){
+        $(this).toggleClass("clicked");
+    });
+
+    // Chart.platform.disableCSSInjection = true;
+    //chart
+    // Any of the following formats may be used
+    let canvas = document.getElementById("myChart");
+    let ctx = canvas.getContext("2d");
+    let masPopChart = new Chart(myChart , {
+        type: 'line',
+        data: {
+            labels: ['0', '1' , '2', '3', '4'],
+            datasets: [{
+                label: null,
+                data: [
+                    16000,
+                    10000, 
+                    20000,
+                    15000,
+                    25000
+                ],
+                backgroundColor: "",
+                borderColor: "#2482C2",
+                borderWidth: 2,
+                steppedLine: false
+            }],
+
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    display: false,
+                }],
+                yAxes: [{
+                    display: false,
+                }]
+            },
+            tooltips: {
+                enabled: false
+            }
+        }
+    });
+})
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
 from django.urls import path, include
 from .views import filter_form_render_view, filter_handle, seed_db
+from django.conf.urls.static import static
+
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', filter_form_render_view, name='filter-render'),
     path('save', filter_handle, name='filter-handle'),
-    path('seed-db', seed_db, name='seed-db')
+    path('seed-db', seed_db, name='seed-db'),
+    path('home', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
